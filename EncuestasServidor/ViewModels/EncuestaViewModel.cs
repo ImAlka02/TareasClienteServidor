b4400d43a439197d137032a5f2a4fe26c18cead9
VICTOR OSVALDO RODRIGUEZ HIDALGO <201G0283@rcarbonifera.tecnm.mx>
2024-04-11 19:43:42 -0600

    casi acabado

--- EncuestasServidor/ViewModels/EncuestaViewModel.cs
+++ EncuestasServidor/ViewModels/EncuestaViewModel.cs
@@ -16,6 +16,37 @@ namespace EncuestasServidor.ViewModels
 	{
 		EncuestaServer server = new();
 
+		public int TotalEncuestados { get; set; } = 1;
+		public decimal Preguntaunosuma { get; set; } = 1;
+		private decimal Preguntaunopromedio;
+
+		public decimal preguntaunopromedio
+		{
+			get { return (Preguntaunopromedio / Preguntadossuma )*40; }
+			set { Preguntaunopromedio = value; }
+		}
+
+		public decimal Preguntadossuma { get; set; } = 1;
+
+		private decimal Preguntadospromedio;
+
+		public decimal preguntadospromedio
+		{
+			get { return (Preguntadospromedio/ TotalEncuestados)*40; }
+			set { Preguntadospromedio = value; }
+		}
+
+
+		public decimal Preguntatressuma { get; set; } = 1;
+		private decimal Preguntatrespromedio;
+
+		public decimal preguntatrespromedio
+		{
+			get { return (Preguntatrespromedio/TotalEncuestados)*40; }
+			set { Preguntatrespromedio = value; }
+		}
+
+
 
 		public EncuestaModel encuesta { get; set; } = new();
 
@@ -30,12 +61,16 @@ namespace EncuestasServidor.ViewModels
 		private void Server_ResultadoObtenido(object? sender, Models.EncuestaModel e)
 		{
 			encuesta.pregunta1 = encuesta.pregunta1 + e.pregunta1;
+			Preguntaunosuma = encuesta.pregunta1;
 			encuesta.pregunta2 = encuesta.pregunta2 + e.pregunta2;
+			Preguntadossuma = encuesta.pregunta2;
 			encuesta.pregunta3 = encuesta.pregunta3 + e.pregunta3;
+			Preguntatressuma = encuesta.pregunta3;
+			TotalEncuestados++;
 
 			PropertyChanged?.Invoke(this, new(nameof(encuesta)));
-
-		}
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
 		public event PropertyChangedEventHandler? PropertyChanged;
     }
 }
